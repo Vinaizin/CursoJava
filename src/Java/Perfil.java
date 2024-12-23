14b2b518a5d2659ae841696d52296f83b768a24d
Vinaizin <Vinicyus_vieira2016@hotmail.com>
2024-12-23 03:41:48 -0300

    CursoJava.inicio

---
+++ src/Java/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Ola vinicyus");
+        System.out.println("Tudo bem?");
+
+    }
+}
---
+++ src/Java/Subtração.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class Subtração {
+    public static void main(String[] args) {
+        System.out.println(10-5);
+    }
+
+}
